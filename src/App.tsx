b18cf13f043300310f0a8b2bd9aff13db1f1c2c4
Soham Pal <sohampal1108@gmail.com>
2024-05-26 12:13:14 +0530

    Implementing Home Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,53 @@
 /** @format */
 
-import React, { useState } from "react";
-import JsonInput from "./components/JsonInput";
-import SchemaInput from "./components/SchemaInput";
-import ValidationResult from "./components/ValidationResult";
-import { JsonSchemaContextProvider } from "./context/JsonSchemaContext";
-import useValidation from "./hooks/useValidation";
+import React from "react";
+import HomePage from "./pages/HomePage";
 
 const App: React.FC = () => {
-    const [json, setJson] = useState<string>("");
-    const [schema, setSchema] = useState<string>("");
-    const { validationResult, validateJson } = useValidation();
-
-    const handleJsonChange = (newJson: string) => {
-        setJson(newJson);
-        validateJson(newJson, schema);
-    };
-
-    const handleSchemaChange = (newSchema: string) => {
-        setSchema(newSchema);
-        validateJson(json, newSchema);
-    };
-
     return (
-        <JsonSchemaContextProvider>
-            <div>
-                <h1>JSON Validator</h1>
-                <SchemaInput onSchemaChange={handleSchemaChange} />
-                <JsonInput onJsonChange={handleJsonChange} />
-                <ValidationResult
-                    isValid={validationResult.isValid}
-                    errors={validationResult.errors}
-                />
-            </div>
-        </JsonSchemaContextProvider>
+        <>
+            <HomePage />
+        </>
     );
 };
 
 export default App;
+
+// import React, { useState } from "react";
+// import JsonInput from "./components/JsonInput";
+// import SchemaInput from "./components/SchemaInput";
+// import ValidationResult from "./components/ValidationResult";
+// import { JsonSchemaContextProvider } from "./context/JsonSchemaContext";
+// import useValidation from "./hooks/useValidation";
+
+// const App: React.FC = () => {
+//     const [json, setJson] = useState<string>("");
+//     const [schema, setSchema] = useState<string>("");
+//     const { validationResult, validateJson } = useValidation();
+
+//     const handleJsonChange = (newJson: string) => {
+//         setJson(newJson);
+//         validateJson(newJson, schema);
+//     };
+
+//     const handleSchemaChange = (newSchema: string) => {
+//         setSchema(newSchema);
+//         validateJson(json, newSchema);
+//     };
+
+//     return (
+//         <JsonSchemaContextProvider>
+//             <div>
+//                 <h1>JSON Validator</h1>
+//                 <SchemaInput onSchemaChange={handleSchemaChange} />
+//                 <JsonInput onJsonChange={handleJsonChange} />
+//                 <ValidationResult
+//                     isValid={validationResult.isValid}
+//                     errors={validationResult.errors}
+//                 />
+//             </div>
+//         </JsonSchemaContextProvider>
+//     );
+// };
+
+// export default App;
